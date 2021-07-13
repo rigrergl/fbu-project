d8c043589af65fa2eb066a44eb0e47d175ed4530
Rigre Reinier Garciandia Larquin <rigrergl@fb.com>
2021-07-13 18:44:52 -0500

    removed test post to object score. Added like posting helper method

--- fbu-project/fbu-project/Models/Like.h
+++ fbu-project/fbu-project/Models/Like.h
@@ -14,6 +14,8 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, strong) PFUser *_Nonnull originUser;
 @property (nonatomic, strong) PFUser *_Nonnull destinationUser;
 
++ (void)postLikeFrom:(PFUser *_Nonnull)originUser to:(PFUser *_Nonnull)destinationUser withCompletion:(PFBooleanResultBlock  _Nullable)completion;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- fbu-project/fbu-project/Models/Like.m
+++ fbu-project/fbu-project/Models/Like.m
@@ -13,4 +13,12 @@
     return @"Like";
 }
 
++ (void)postLikeFrom:(PFUser *_Nonnull)originUser to:(PFUser *_Nonnull)destinationUser withCompletion:(PFBooleanResultBlock _Nullable)completion {
+    Like *newLike = [Like new];
+    newLike.originUser = originUser;
+    newLike.destinationUser = destinationUser;
+    
+    [newLike saveInBackgroundWithBlock:completion];
+}
+
 @end
