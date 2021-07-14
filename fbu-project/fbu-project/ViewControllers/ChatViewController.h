51ca15cc5d8a84c1453a1d35f0240e9cf489bd27
Rigre Reinier Garciandia Larquin <rigrergl@fb.com>
2021-07-14 11:23:24 -0500

    setup chat basic autolayout

---
+++ fbu-project/fbu-project/ViewControllers/ChatViewController.h
@@ -0,0 +1,16 @@
+//
+//  ChatViewController.h
+//  fbu-project
+//
+//  Created by Rigre Reinier Garciandia Larquin on 7/14/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChatViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
