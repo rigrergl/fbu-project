374ee3598ea1f6822d34d4daf94e6005c5c5150f
Rigre Reinier Garciandia Larquin <rigrergl@fb.com>
2021-07-15 18:16:14 -0500

    removed leftover playback code from CustomDraggableView

--- fbu-project/fbu-project/Views/CustomDraggableView.m
+++ fbu-project/fbu-project/Views/CustomDraggableView.m
@@ -6,17 +6,16 @@
 //
 
 #import "CustomDraggableView.h"
-#import <AVFoundation/AVFoundation.h> //TODO: REMOVE THIS IMPORT
 #import "MediaPlayBackView.h"
 #import "Like.h"
 #import "UnLike.h"
 
-@interface CustomDraggableView () <AVAudioPlayerDelegate>
+@interface CustomDraggableView ()
 
-@property (nonatomic, strong) AVAudioPlayer *audioPlayer;
+
 @property (nonatomic, strong) PFUser *user;
-@property (nonatomic, strong) UIProgressView *progressView;
-@property (nonatomic, strong) NSTimer *progressTimer;
+@property (nonatomic, strong) MediaPlayBackView *playbackView;
+
 
 @end
 
@@ -53,6 +52,7 @@
             MediaPlayBackView *playbackView = [[MediaPlayBackView alloc]
                                                initWithFrame:CGRectMake(20, 200, self.frame.size.width - 40, 200)
                                                andData:data];
+            self.playbackView = playbackView;
             [self addSubview:playbackView];
         }
     }];
@@ -67,123 +67,9 @@
     [self addSubview:self.usernameLabel];
 }
 
-- (void)setupPlayButton {
-    CGFloat buttonWidth = 50;
-    self.playButton = [[UIButton alloc] initWithFrame:CGRectMake(self.frame.size.width/2 - buttonWidth/2, 200, buttonWidth, buttonWidth)];
-    [self.playButton setTitle:@"" forState:UIControlStateNormal];
-    [self.playButton setBackgroundImage:[UIImage systemImageNamed:@"play"] forState:UIControlStateNormal];
-    [self.playButton setBackgroundImage:[UIImage systemImageNamed:@"stop"] forState:UIControlStateSelected];
-    [self.playButton addTarget:self action:@selector(didTapPlayButton) forControlEvents:UIControlEventTouchUpInside];
-    
-    [self addSubview:self.playButton];
-}
-
-- (void)didTapPlayButton {
-    if (self.playButton == nil) {
-        NSLog(@"Error: nil play button");
-        return;
-    }
-    
-    if ([self.playButton isSelected]) {
-        [self stopPlaying];
-    } else {
-        [self playRecording];
-    }
-}
-
-
 - (void)afterSwipeAction {
-    [self stopPlaying];
+    [self.playbackView stopPlaying];
     [super afterSwipeAction];
-}
-
-#pragma mark - Recording playback
-
-- (void)showProgressView {
-    CGFloat barWidth = 300;
-    self.progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(self.frame.size.width/2 - barWidth/2, 300, barWidth, 50)];
-    self.progressView.progress = 0.5;
- 
-    [self addSubview:self.progressView];
-    
-    self.progressView.alpha = 0;
-    [UIView animateWithDuration:.5 animations:^{
-        self.progressView.alpha = 1;
-    }];
-    
-    self.progressTimer = [NSTimer scheduledTimerWithTimeInterval:0.05 target:self selector:@selector(updatePlaybackSlider:) userInfo:nil repeats:YES];
-}
-
-- (void)hideProgressView {
-    [self.progressTimer invalidate];
-    self.progressTimer = nil;
-    [self.progressView removeFromSuperview];
-    self.progressView = nil;
-}
-
-- (void)updatePlaybackSlider:(NSTimer *)timer {
-    if (self.audioPlayer == nil || self.progressView == nil) {
-        NSLog(@"Not ready for progress bar");
-    }
-    
-    float totalTime = self.audioPlayer.duration;
-    float currentTime = self.audioPlayer.currentTime / totalTime;
-    
-    self.progressView.progress = currentTime;
-}
-
-- (void)playRecording
-{
-    
-    PFFileObject *recordingFile = self.user[@"recording"];
-    if(recordingFile == nil) {
-        return;
-    }
-    
-    [self.playButton setSelected:YES];
-    
-    [recordingFile getDataInBackgroundWithBlock:^(NSData *_Nullable data, NSError *_Nullable error){
-        if (error) {
-            NSLog(@"Error getting data from PFFileObject");
-        } else {
-            [self playRecordingWithData:data];
-        }
-    }];
-}
-
-- (void)playRecordingWithData:(NSData *)data {
-    AVAudioSession *session = [AVAudioSession sharedInstance];
-    [session setCategory:AVAudioSessionCategoryPlayback error:nil];
-    
-    NSError *error = nil;
-    self.audioPlayer = [[AVAudioPlayer alloc] initWithData:data error:&error];
-    self.audioPlayer.delegate = self;
-    self.audioPlayer.numberOfLoops = 0;
-    [self.audioPlayer play];
-    [self showProgressView];
-}
-
-
-- (void)stopPlaying {
-    [self.playButton setSelected:NO];
-    
-    if (self.progressView) {
-        [self hideProgressView];
-    }
-    
-    if (self.audioPlayer) {
-        [self.audioPlayer stop];
-        self.audioPlayer = nil;
-    }
-}
-
-- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag {
-    
-    if (flag) {
-        [self stopPlaying];
-    } else {
-        NSLog(@"audioPlayerDidFinishPlaying with error");
-    }
 }
 
 #pragma mark - Card Swiped Override Methods
--- fbu-project/fbu-project/Views/MediaPlayBackView.h
+++ fbu-project/fbu-project/Views/MediaPlayBackView.h
@@ -12,6 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface MediaPlayBackView : UIView
 
 - (id)initWithFrame:(CGRect)frame andData:(NSData *_Nonnull)data;
+- (void)stopPlaying;
 
 @end
 
